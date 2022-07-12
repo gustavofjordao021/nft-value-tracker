22605c4965a4aca904de6f9e9fece1aa25300174
Gustavo Jordao <gustavo.jordao021@gmail.com>
2022-07-12 16:02:24 -0400

    Fix GA setup

--- pages/_app.js
+++ pages/_app.js
@@ -58,16 +58,12 @@ function MyApp({ Component, pageProps }) {
 				strategy="lazyOnload"
 				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
 			/>
-
 			<Script strategy="lazyOnload">
-				{`
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
-                    });
-                `}
+				{`window.dataLayer = window.dataLayer || [];
+  				function gtag(){dataLayer.push(arguments);}
+  				gtag('js', new Date());
+
+  				gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`}
 			</Script>
 			<Provider
 				autoConnect
